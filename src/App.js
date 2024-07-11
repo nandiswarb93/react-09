b8163f8fb9cd7fc9485d46272ec5ecc7de59d7db
Busapalli Nandiswar <nandiswarb93@gmail.com>
2024-07-11 09:15:41 +0530

    ipl task jul 10

--- src/App.js
+++ src/App.js
@@ -9,20 +9,53 @@ import Orderedlist from './component/olist';
 import Customimage from './component/image';
 
 
-function App() {
-  return (
-    <div>
-      <h1>hello</h1>
-      <LoginButton > </LoginButton>
-      <Cards />
-      <Table/>
-      <Unorderedlist/>
-      <Orderedlist/>
-      <Customimage/>
-      
-      
-    </div>
-  );
-}
+import IplData from './ipl/ipldata';
+import IplImage from './ipl/iplimage';
+import IplHeading from './ipl/iplheading';
+import Iplcups from './ipl/iplcups';
+import Iplplayers from './ipl/iplplayers';
+import Iplrunners from './ipl/iplrunners';
 
+const App = () => (
+  <div>
+
+    {IplData.map((eachImg) => (
+      
+      <div  key={eachImg.team} style={{height:"600px",  width:"500px", border:"2px solid black", display:'inline-block'}}>
+         
+        <IplHeading team={eachImg.team}/>
+        <IplImage
+          source={eachImg.jerseyimg}
+          alternate="ipl image"
+          width={300}
+          height={300}
+        />
+        <Iplcups cups={eachImg.cups}/>
+        <Iplplayers players={eachImg.players}/>
+
+        <Iplrunners runners={eachImg.runners}/>
+
+
+      </div>
+    ))}
+  </div>
+);
+
+
+
+// function App = ()= {
+//   return (
+//     <div>
+//       {/* <h1>hello</h1>
+//       <LoginButton > </LoginButton>
+//       <Cards />
+//       <Table/>
+//       <Unorderedlist/>
+//       <Orderedlist/>
+//       <Customimage/> */}
+//  <iplData/>    
+//     </div>
+//   );
+// }
 export default App;
+
