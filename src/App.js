0f339258f1e1ac9d9732ffe3742ebba145b49b30
Busapalli Nandiswar <nandiswarb93@gmail.com>
2024-07-21 22:02:20 +0530

    some update are made

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
-
-import { UnorderList } from './data/list';
-import data from './data/data';
-import { Heading } from './data/heading';
-import { Image } from './data/image';
-import { Name } from './data/name';
+import "./App.css";
+import { UnorderList } from "./data/list";
+import data from "./data/data";
+import { Heading } from "./data/heading";
+import { Image } from "./data/image";
+import { Name } from "./data/name";
 import "./data/style.css";
 import Conditionalrendering, {
   ConditionRender,
 } from "./conditionalrendering/conditionalrender";
-
 const arr = [1, 2, 3, 4, 5, 6, 6, 8];
 
 function App() {
--- src/conditionalrendering/conditionalrender.js
+++ src/conditionalrendering/conditionalrender.js
@@ -1,19 +1,60 @@
 
-import "../data/style.css"
+// import "../data/style.css"
 
-const Conditionalrendering = (prop) => {
-  const { array } = prop;
+// const Conditionalrendering = (prop) => {
+//   const { array } = prop;
+
+//   return (
+//     <>
+//       {array.length === 0 ? (
+//         <h2>array length is 0</h2>
+//       ) : (
+//         <h2> array length is {`${array.length}`}</h2>
+//       )}
+//     </>
+//   );
+// };
+// export default Conditionalrendering;
+
+
+
+
+// export const ConditionRender = (props) => {
+//   const { array } = props;
+
+//   return (
+//     <>
+//       {array.map((data, index) => (
+//         data % 2 === 0 ? (
+//           <h3 key={index} className="container1">the array item is even</h3>
+//         ) : (
+//           <h3 key={index} className="container2">the array item is odd</h3>
+//         )
+//       ))}
+//     </>
+//   );
+// };
+
+
+
+
+
+import "../data/style.css";
+
+const Conditionalrendering = (props) => {
+  const { array } = props;
 
   return (
     <>
-      {array.length == 0 ? (
-        <h2>array length is 0</h2>
+      {array.length === 0 ? (
+        <h2>Array length is 0</h2>
       ) : (
-        <h2> array length is {`${array.length}`}</h2>
+        <h2>Array length is {array.length}</h2>
       )}
     </>
   );
 };
+
 export default Conditionalrendering;
 
 
@@ -26,9 +67,9 @@ export const ConditionRender = (props) => {
     <>
       {array.map((data, index) => (
         data % 2 === 0 ? (
-          <h3 key={index} className="container1">the array item is even</h3>
+          <h3 key={index} className="container1">The array item is even</h3>
         ) : (
-          <h3 key={index} className="container2">the array item is odd</h3>
+          <h3 key={index} className="container2">The array item is odd</h3>
         )
       ))}
     </>
