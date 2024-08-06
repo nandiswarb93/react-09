07d413ced8879d82e1c320993507aef7cd7d44fe
Busapalli Nandiswar <nandiswarb93@gmail.com>
2024-08-06 23:57:42 +0530

    task modified

--- src/navigation/settings.js
+++ src/navigation/settings.js
@@ -1,3 +1,37 @@
+// import NavBar from "./navbar";
+// import { useContext, useState } from "react";
+
+// import { MyContext } from "./navigation";
+
+// const SettingScreen = () => {
+//   const { username, darkMode, changeAccName } = useContext(MyContext);
+
+//   const [userEntered, setUserEntered] = useState("");
+
+//   const userHandler = (e) => {
+//     e.preventDefault();
+//     setUserEntered(e.target.value);
+//   };
+//   const submitHandler = () => {
+//     changeAccName(userEntered);
+//   };
+//   const themeHandler = () => {};
+//   return (
+//     <>
+//       <NavBar />
+//       <h3>Welcome {userEntered}</h3>
+//       <h4>want to change user name</h4>
+//       <input type="text" value={userEntered} onChange={userHandler}></input>
+//       <button onClick={submitHandler}>submit</button>
+
+//       <h1>hiiii this is setting screen</h1>
+
+//       <button onClick={themeHandler}>click to change theme</button>
+//     </>
+//   );
+// };
+// export default SettingScreen;
+
 import NavBar from "./navbar";
 import { useContext, useState } from "react";
 
